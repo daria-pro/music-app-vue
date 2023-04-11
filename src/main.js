b011d7419d3376949b02bbb9e3ca665cd1d0802a
daria-pro <shaulkod@gmail.com>
2023-04-11 14:29:19 +0300

    added i18n

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import i18n from './includes/i18n'
 
 import App from './App.vue'
 import router from './router'
 import VeeValidatePlugin from './includes/validation'
 import { auth } from './includes/firebase'
-
+import Icon from './directives/icon'
 import './assets/base.css'
 import './assets/main.css'
 
@@ -18,6 +19,8 @@ auth.onAuthStateChanged(() => {
     app.use(createPinia())
     app.use(router)
     app.use(VeeValidatePlugin)
+    app.use(i18n)
+    app.directive('icon', Icon)
 
     app.mount('#app')
   }
